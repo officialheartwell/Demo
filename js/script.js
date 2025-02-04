6249bd8b7adfcd7f8408291f6a1bcac3a3559ffc
officialheartwell <okorieheartwell@gmail.com>
2025-02-04 12:30:49 +0100

    build an animated css hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 "use strick";
+const menuBtn = document.querySelector(".menu-btn");
+
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("open");
+  console.log(menuBtn);
+});
+
